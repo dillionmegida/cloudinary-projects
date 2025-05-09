0613c77084e93c9586722ae7ff4b50b0e4791ad4
Dillion Megida <dillionmegida@gmail.com>
2025-05-09 21:32:42 +0200

    add svelte ai projects

---
+++ svelte-ai-bg-removal/src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+	import { CldImage } from "svelte-cloudinary";
+	import "../lib/styles.css";
+
+  const demoImage = {
+    truck: {
+      src: 'demo/car.webp',
+      alt: 'Teal colored truck in a desert'
+    },
+    "white-sneaker": {
+      src: 'demo/white-adidas-sneaker.jpg',
+      alt: 'White Adidas sneaker'
+    },
+    "red-sneaker": {
+      src: 'demo/red-nike-sneaker.jpg',
+      alt: 'Red Nike sneaker'
+    }
+  }
+
+  const currentImageKey = 'red-sneaker'
+</script>
+
+<div class="container">
+	<h1>AI Background Removal</h1>
+	<p>Remove backgrounds from images using AI</p>
+
+	<div class="images-column">
+		<!-- Basic Background Removal -->
+		<!-- <div>
+			<span class="image-label">Original</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="800"
+					alt={demoImage[currentImageKey].alt}
+				/>
+			</div>
+		</div>
+		<div>
+			<span class="image-label">Bg Removed</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="800"
+					alt={demoImage[currentImageKey].alt}
+					removeBackground={true}
+				/>
+			</div>
+		</div>
+	</div> -->
+
+		<div>
+			<span class="image-label">Original</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="600"
+					alt={demoImage[currentImageKey].alt}
+					gravity="auto"
+					crop="fill"
+				/>
+			</div>
+		</div>
+		<div>
+			<span class="image-label">Bg Removed</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="600"
+					alt={demoImage[currentImageKey].alt}
+					removeBackground={true}
+					gravity="auto"
+          crop="fill"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ svelte-ai-smart-cropping/src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { CldImage } from "svelte-cloudinary";
+	import "../lib/styles.css";
+
+  const demoImage = {
+    "white-adidas-sneaker": {
+      src: 'demo/white-adidas-sneaker.jpg',
+      alt: 'White Adidas sneaker'
+    },
+    "plant-in-a-vase": {
+      src: 'demo/plant-in-a-vase.jpg',
+      alt: 'Plant in a vase'
+    },
+    "airspun-cream-with-flowers": {
+      src: 'demo/airspun-cream-with-flowers.jpg',
+      alt: 'Airspun cream with flowers'
+    }
+  }
+
+  const currentImageKey = 'plant-in-a-vase'
+</script>
+
+<div class="container">
+	<h1>AI Smart Cropping</h1>
+	<p>Smart crop images using Cloudinary AI</p>
+
+	<div class="images-column">
+		<div>
+			<span class="image-label">Original</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="800"
+					alt={demoImage[currentImageKey].alt}
+				/>
+			</div>
+		</div>
+		<div>
+			<span class="image-label">Smart Cropped</span>
+			<div class="image-container">
+				<CldImage
+					src={demoImage[currentImageKey].src}
+					width="800"
+					height="800"
+					alt={demoImage[currentImageKey].alt}
+          gravity="auto"
+          crop="auto"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
