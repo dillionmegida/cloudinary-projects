eb9904c693f70ef4cd8ea97097460099690ba11a
Dillion Megida <dillionmegida@gmail.com>
2025-05-16 16:19:19 +0200

    add loading indicator during bg removal

--- svelte-ai-bg-removal/src/routes/+page.svelte
+++ svelte-ai-bg-removal/src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script>
 	import { CldImage } from "svelte-cloudinary";
+  import { onMount } from 'svelte';
 	import "../lib/styles.css";
+
+  onMount(() => {
+    function cleanup() {
+      const customImages = document.querySelectorAll('.custom-image img');
+
+      customImages.forEach(image => {
+        image.addEventListener('load', () => {
+          console.log(image.parentElement)
+          image.parentElement?.classList.remove('loading-indicator')
+        });
+      });
+    }
+
+    cleanup()
+
+    return () => {
+      cleanup();
+    };
+  });
 
   const demoImage = {
     truck: {
@@ -22,11 +42,10 @@
 
 <div class="container">
 	<h1>AI Background Removal</h1>
-	<p>Remove backgrounds from images using AI</p>
+	<p>Remove backgrounds from images using AI</p>  
 
 	<div class="images-column">
-		<!-- Basic Background Removal -->
-		<!-- <div>
+		<div>
 			<span class="image-label">Original</span>
 			<div class="image-container">
 				<CldImage
@@ -39,7 +58,7 @@
 		</div>
 		<div>
 			<span class="image-label">Bg Removed</span>
-			<div class="image-container">
+			<div class="image-container custom-image loading-indicator">
 				<CldImage
 					src={demoImage[currentImageKey].src}
 					width="800"
@@ -49,9 +68,10 @@
 				/>
 			</div>
 		</div>
-	</div> -->
+	</div>
+  
 
-		<div>
+		<!-- <div>
 			<span class="image-label">Original</span>
 			<div class="image-container">
 				<CldImage
@@ -77,6 +97,6 @@
           crop="fill"
 				/>
 			</div>
-		</div>
-	</div>
+		</div> -->
+	<!-- </div> -->
 </div>
