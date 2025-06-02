3a2fa0d11de903b5ca8e2cc61f7d5c9d46d3eed7
Dillion Megida <dillionmegida@gmail.com>
2025-06-02 08:42:05 +0200

    use fillbackground in the sdk

--- svelte-ai-generative-fill/src/routes/+page.svelte
+++ svelte-ai-generative-fill/src/routes/+page.svelte
@@ -3,10 +3,11 @@
   import { generativeFill } from '@cloudinary/url-gen/qualifiers/background';
   import { Cloudinary } from '@cloudinary/url-gen';
   import { pad } from "@cloudinary/url-gen/actions/resize";
+  import { CldImage } from "svelte-cloudinary";
 
-  const cld = new Cloudinary({
-    cloud: { cloudName: 'dillionmegida' }
-  });
+  // const cld = new Cloudinary({
+  //   cloud: { cloudName: 'dillionmegida' }
+  // });
 
 // Example of how to use generative fill with a CldImage component
 
@@ -18,19 +19,19 @@
     },
     "nice-red-shoe": {
       src: 'demo/nice-red-shoe.jpg',
-      alt: 'A red nike sneaker',
+      alt: 'A nice red shoe',
       vertical: true
     }
   }
 
   const currentImageKey = 'nice-red-shoe'
 
-  const myImage = cld.image(demoImage[currentImageKey].src).resize(
-  pad()
-    .height(1600)
-    .width(900)
-    .background(generativeFill())
-).toURL();
+//   const myImage = cld.image(demoImage[currentImageKey].src).resize(
+//   pad()
+//     .height(1600)
+//     .width(900)
+//     .background(generativeFill().prompt('A futuristic cityscape'))
+// ).toURL();
 </script>
 
 <div class="container">
@@ -40,7 +41,15 @@
 	<div class="images-column">
     <div class="image-container">
       <div>
-          <img class={demoImage[currentImageKey].vertical ? 'vertical' : ''} src={myImage} alt={demoImage[currentImageKey].alt} />
+        <CldImage
+        width={1080}
+        height={1920}
+        alt={demoImage[currentImageKey].alt}
+        src={demoImage[currentImageKey].src}
+        fillBackground
+        crop='pad'
+        />  
+          <!-- <img class={demoImage[currentImageKey].vertical ? 'vertical' : ''} src={myImage} alt={demoImage[currentImageKey].alt} /> -->
       </div>
     </div>
 	</div>
